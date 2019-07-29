1a47e463d1ee113d12e3ff330308d1e7f71d91f6
Ryujeuk <jeuk817@gmail.com>
2019-07-29 19:55:37 +0900

    feat: 서버와 mongoDB연결, 회원가입페이지와 서버 연결
    1. mongoose를 이용해 서버와 mongoDB연결
    2. public/javascripts/signup.js를 생성하여 회원가입페이지의
    계정생성버튼에 서버로 id, pwd를 넘기도록 구현
    3. client에서 넘어온 req.body를 express.json()로 파싱하도록 구현
    4. url='/users/signUP' method=post로 요청이 오면, id,pwd를 받아서 db에
    저장하는 router 구현

--- news-site/app.js
+++ news-site/app.js
@@ -16,8 +16,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser('secret code'));
 app.use(session({
--- news-site/routes/users.js
+++ news-site/routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const DB = require('../schemas/index');
+const db = new DB();
+const testC = db.userCollection();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/signUp', (req, res, next) => {
+  const { id, pwd } = req.body;
+  db.connect();
+  let test = new testC({ id, pwd });
+  test.save((err, account) => {
+    if (err) return console.error(err);
+  })
+})
 
 module.exports = router;

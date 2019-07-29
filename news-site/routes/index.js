e5f99d501e15f192efa097016b603e5603e410e9
Ryujeuk <jeuk817@gmail.com>
2019-07-29 14:21:33 +0900

    feat: 회원가입페이지 구현
    1. 로그인페이지를 extends와 block을 이용해 나눔.
    2. singup.pug를 만들어 extends sign 하여 회원가입페이지 구현

--- news-site/routes/index.js
+++ news-site/routes/index.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/sign', function (req, res, next) {
+router.get('/login', function (req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.render('sign');
+  res.render('login');
+});
+
+router.get('/signUp', function (req, res, next) {
+  res.render('signup');
 });
 
 module.exports = router;

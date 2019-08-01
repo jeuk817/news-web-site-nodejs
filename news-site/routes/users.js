8bbfb9952e7de259ec309b8d98690485c957d3b8
Ryujeuk <jeuk817@gmail.com>
2019-08-01 22:14:47 +0900

    feat: passport를 이용한 로그인 로그아웃 구현
    1. passport를 적용하느라 중간 커밋을 하지못함.
    2. 그래서 주석으로 상세설명추가

--- news-site/routes/users.js
+++ news-site/routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 
 const UserCollection = require('../schemas/user');
 
@@ -34,5 +36,36 @@ router.post('/identification', isNotLoggedIn, async (req, res, next) => {
     next(err);
   }
 })
+
+// 로그인버튼 클릭 시 실행되는 함수
+// app.js: passportConfig(passport) -> passport/index.js: local(passport) -> passport/localStrategy.js: passport.use -> 여기
+// passport.use에서 로그인 성공/실패에 따라 다른 매개변수가 passport.authenticate의 두번째 콜백함수에 넘어옵니다.
+// 실패시 로그인페이지에 redirect 성공시 홈으로 redirect
+router.post('/login', isNotLoggedIn, (req, res, next) => {
+  passport.authenticate('local', (authError, user, info) => {
+    if (authError) {
+      console.error(authError);
+      return next(authError);
+    }
+    if (!user) {
+      return res.redirect('/loginPage');
+    }
+    return req.login(user, (loginError) => {
+      if (loginError) {
+        console.error(loginError);
+        return next(loginError);
+      }
+      return res.redirect('/')
+    });
+  })(req, res, next);
+});
+
+// 로그아웃버튼 클릭시 실행
+// req.logout은 req.user를 없애고, req.session.destroy는 req.session을 없앱니다. 그리고 홈페이지로 redirect합니다.
+router.get('/logout', isLoggedIn, (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.redirect('/');
+});
 
 module.exports = router;

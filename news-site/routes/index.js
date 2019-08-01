5e80cb12790f47d6bf682761875066a76de0a831
Ryujeuk <jeuk817@gmail.com>
2019-08-01 21:42:15 +0900

    feat: 홈페이지 생성, 페이지 이동 구현
    1. 홈페이지로 쓸 homepage.pug와 homepage.css생성
    2. 홈페이지에서 로그인버튼 -> 로그인페이지로 연결
    3. 그외 홈페이지 로그인페이지 회원가입페이지 이동 구현

--- news-site/routes/index.js
+++ news-site/routes/index.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/login', function (req, res, next) {
-  // res.render('index', { title: 'Express' });
+// 홈페이지
+router.get('/', (req, res, next) => {
+  if (req.isAuthenticated()) return next();
+  res.render('homepage');
+});
+
+// 로그인페이지
+router.get('/loginPage', isNotLoggedIn, function (req, res, next) {
   res.render('login');
 });
 
-router.get('/signUp', function (req, res, next) {
+// 회원가입페이지
+router.get('/signUp', isNotLoggedIn, function (req, res, next) {
   res.render('signup');
 });
 

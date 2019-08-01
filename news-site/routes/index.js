a96c30f3ba34f6cb0c5a25a6d9b8ac1f0a401632
Ryujeuk <jeuk817@gmail.com>
2019-08-01 21:53:15 +0900

    Revert "feat: 홈페이지 생성, 페이지 이동 구현"
    This reverts commit 5e80cb12790f47d6bf682761875066a76de0a831.

--- news-site/routes/index.js
+++ news-site/routes/index.js
@@ -1,19 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-// 홈페이지
-router.get('/', (req, res, next) => {
-  if (req.isAuthenticated()) return next();
-  res.render('homepage');
-});
-
-// 로그인페이지
-router.get('/loginPage', isNotLoggedIn, function (req, res, next) {
+/* GET home page. */
+router.get('/login', function (req, res, next) {
+  // res.render('index', { title: 'Express' });
   res.render('login');
 });
 
-// 회원가입페이지
-router.get('/signUp', isNotLoggedIn, function (req, res, next) {
+router.get('/signUp', function (req, res, next) {
   res.render('signup');
 });
 

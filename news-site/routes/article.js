8d6092a319c471fd871c7623937b821467b7b7e8
jack <jeuk817@gmail.com>
2019-11-17 10:32:17 +0900

    modified: 비로그인 댓글,감정표현시 미들웨어 loginConfig 대신 isLoggedIn으로 교체

--- news-site/routes/article.js
+++ news-site/routes/article.js
@@ -12,10 +12,7 @@ router.get('/:id', loginConfig, async (req, res, next) => {
 });
 
 // 사용자가 감정표현을 클릭했을 때 실행
-router.post('/emotion', loginConfig, async (req, res, next) => {
-    // 비 로그인 상태일 때는 로그인페이지 url을 리다이렉트한다.
-    if(!req.user) return res.redirect('/loginPage');
-
+router.post('/emotion', isLoggedIn, async (req, res, next) => {
     // 사용자가 클릭한 감정표현과 기사의 _id를 받는다.
     const { emotion, article_id } = req.body;
     // db에 감정표현을 업데이트하고 변경된 감정표현들의 수를 배열로 받는다.
@@ -23,13 +20,14 @@ router.post('/emotion', loginConfig, async (req, res, next) => {
     res.send(updatedEmotions);
 });
 
-router.post('/comment', loginConfig, async (req, res, next) => {
-    // 비 로그인 상태일 때는 로그인페이지 url을 리다이렉트한다.
-    if(!req.user) return res.redirect('/loginPage');
-
+router.post('/comment', isLoggedIn, async (req, res, next) => {
     const {article_id, content} = req.body;
     const updatedCommentsObject = await newsEditor.createComment({article_id, user_id: req.user._id, content, displayName: req.user.displayName});
     res.json(updatedCommentsObject);
+})
+
+router.get('/userArticles', isLoggedIn, async (req, res, next) => {
+
 })
 
 module.exports = router;

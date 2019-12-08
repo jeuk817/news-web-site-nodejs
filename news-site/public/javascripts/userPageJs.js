b0eb0f183a70f6c31865d678781a7243bcfb5ad7
jack <jeuk817@gmail.com>
2019-12-08 20:44:22 +0900

    refactor: 기사쓰기 ajax로 변경, 변수명 변경
    1. 기사쓰기 클릭 시 새로운 페이지를 로드하지않고 ajax로 처리하도록 변경
    2. element관련 변수이름을 좀더 직관적으로 변경

--- news-site/public/javascripts/userPageJs.js
+++ news-site/public/javascripts/userPageJs.js
@@ -2,30 +2,91 @@ const withdrawal = document.getElementById('withdrawal');
 const writtenArticles = document.getElementById('writtenArticles');
 const menu = document.getElementById('menu');
 const content = document.getElementById('content');
+const writeArticle = document.getElementById('writeArticle');
 
+// element를 만드는 함수
 function createElementFunc({tag='div', className='', id='', text=''}){
-    let div = document.createElement(tag);
-    div.className = className;
-    div.id = id;
+    let element = document.createElement(tag);
+    element.className = className;
+    element.id = id;
     let textnode = document.createTextNode(text);
-    div.appendChild(textnode);
-    return div;
+    element.appendChild(textnode);
+    return element;
 }
 
+// 자신의 쓴 기사 리스트들을 표시하는 함수
 function createArticleList(articles){
     const container = createElementFunc({tag: 'div', id : "userArticlesContainer"});
     articles.forEach((article,i) => {
-        const a = document.createElement('a');
-        a.href = `article/${article._id}`;
+        const anchor = document.createElement('a');
+        anchor.href = `article/${article._id}`;
         const _article = createElementFunc({tag: 'span',className:'userArticles',id: 'article' + (1 + i), text: article.title});
+        const editBtn = createElementFunc({tag: 'button', className: 'editBtns', text: '수정'});
         const br = document.createElement('br');
-        a.appendChild(_article);
-        container.appendChild(a);
+        anchor.appendChild(_article);
+        container.appendChild(anchor);
+        container.appendChild(editBtn);
         container.appendChild(br);
     });
     content.appendChild(container);
 }
 
+// 기사를 입력할 틀을 생성하는 함수
+function createTextFrame(){
+    const textFrame = `
+    <form id="newText" action="/text/write" method="post" enctype="multipart/form-data">
+        <table>
+            <tbody>
+                <tr>
+                    <th>제목</th>
+                    <td>
+                        <input type="text" name="title" id="title" placeholder="제목">
+                    </td>
+                </tr>
+                <tr>
+                    <th>테마</th>
+                    <td>
+                        <select name="thema">
+                            <option value="politics">정치</option>
+                            <option value="economy">경제</option>
+                            <option value="IT">IT</option>
+                            <option value="life">생활</option>
+                            <option value="world">세계</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <th>이미지파일</th>
+                    <td>
+                        <input type="file" name="image">
+                    </td>
+                </tr>
+                <tr>
+                    <th>내용</th>
+                    <td>
+                        <textarea cols="10" name="content" placeholder="내용"></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="button" id="cancel" value="취소">
+                        <input type="submit" value="저장">
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </from>
+    `
+    content.innerHTML = '';
+    content.innerHTML = textFrame;
+}
+
+// '기사쓰기' 클릭 시 실행
+writeArticle.addEventListener('click', (event) => {
+    createTextFrame();
+})
+
+// 회원탈퇴
 withdrawal.addEventListener('click', async (event) => {
     const choice = confirm('정말로 회원탈퇴를 하시겠습니까?');
     if (choice == false) return;
@@ -38,6 +99,7 @@ withdrawal.addEventListener('click', async (event) => {
     }
 })
 
+// '내가 쓴 기사' 클릭 시 실행
 writtenArticles.addEventListener('click', async (event) => {
     const response = await fetch('/user/userArticles', {
         method: 'GET'

12c82b7204bbd9e375e78022af7529f7f1b024f6
jack <jeuk817@gmail.com>
2020-10-19 20:22:10 +0900

    feat: vue project init, create v-app-bar
    1. vue cli 사용해서 vue project 생성
    2. 메인페이지 상단바 생성

---
+++ frontend/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-app>
+    <v-app-bar
+      absolute
+      color="primary"
+      dark
+      inverted-scroll
+      scroll-threshold=200
+      id="fixedBar"
+    >
+      <v-container class="mainWidth mainBar">
+        <!-- <v-spacer></v-spacer> -->
+        <div>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+          <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+        </div>
+        <v-toolbar-title>정치</v-toolbar-title>
+        <v-toolbar-title>경제</v-toolbar-title>
+        <v-toolbar-title>사회</v-toolbar-title>
+        <v-toolbar-title>IT</v-toolbar-title>
+        <v-toolbar-title>세계</v-toolbar-title>
+      </v-container>
+    </v-app-bar>
+    <v-main>
+      <HelloWorld/>
+      <HelloWorld/>
+      <HelloWorld/>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld
+  },
+
+  data: () => ({
+    //
+  })
+}
+</script>
+<style scoped>
+#fixedBar{
+  position: fixed;
+}
+.mainWidth{
+  margin: auto;
+  padding: 0 3%;
+  max-width: 1200px;
+}
+.mainBar{
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  align-items: center;
+}
+</style>
